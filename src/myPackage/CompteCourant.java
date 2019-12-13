5537b3dc205117ea242cf78a52be6824b877ce74
dream <antonin.boureau@hotmail.fr>
2019-12-13 11:11:27 +0100

    Last Development step

--- src/myPackage/CompteCourant.java
+++ src/myPackage/CompteCourant.java
@@ -12,4 +12,12 @@ public class CompteCourant
 	
 	public void addMoney(double m)
 	{ solde += m; }
+	
+	public double takeMoney(double m) throws Exception
+	{
+		if(m > solde)
+			throw new Exception("Solde insuffisant");
+		else
+			return solde -= m;
+	}
 }
